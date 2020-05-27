1a07b0811fbfc5e4776ad744f6213ad4484895da
Alejandro Echeverria <alejandro@echev.com>
2020-05-27 08:45:43 -0400

    feat: usea feature flag to enable/disable restful response

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import flask
 from flask import jsonify
 import random
 from generate_blocks import generate_blocks
+import requests
 
 app = flask.Flask(__name__)
+
+FEATURE_FLAG_API_URL = 'https://dry-brushlands-69779.herokuapp.com/api/feature_flags/'
 
 @app.route('/api/block_models/', methods=['GET'])
 def block_models():
@@ -18,11 +21,16 @@ def block_models():
       "name": "sm3"
     }
   ]
+  if requests.get(FEATURE_FLAG_API_URL).json()['restful_response']:
+    block_models = { "block_models": block_models }
   return jsonify(block_models)
 
 @app.route('/api/block_models/<name>/blocks/', methods=['GET'])
 def blocks(name):
-  return jsonify(generate_blocks(20,20,10))
+  blocks = generate_blocks(20,20,10)
+  if requests.get(FEATURE_FLAG_API_URL).json()['restful_response']:
+    blocks = { "block_model": { "blocks": blocks } }
+  return jsonify(blocks)
 
 @app.after_request
 def add_header(response):
@@ -31,4 +39,4 @@ def add_header(response):
     return response
 
 if __name__ == '__main__':
-  app.run()
+  app.run(port= 8000)
